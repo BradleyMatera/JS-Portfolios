5d881116fe0d680dfd62cc52da50b44c39edc5cb
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-06-01 20:57:54 -0500

    fixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfix
    fixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfixfix

--- reactapptest/src/index.js
+++ reactapptest/src/index.js
@@ -1,13 +1,23 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import { BrowserRouter as Router } from 'react-router-dom';
+=======
+import App from './components/App';
+>>>>>>> Stashed changes
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
+<<<<<<< Updated upstream
   <Router basename="/JS-Portfolios/reactapptest">
     <App />
   </Router>,
+=======
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+>>>>>>> Stashed changes
   document.getElementById('root')
 );
